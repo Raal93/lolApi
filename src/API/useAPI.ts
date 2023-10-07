aeae34036f18d44def74a0f92eabcdfa1ba3d249
Rafał Czajkowski <raal93@gmail.com>
2023-10-07 06:29:44 +0200

    proviing temporary API

--- src/API/useAPI.ts
+++ src/API/useAPI.ts
@@ -1,6 +1,29 @@
 import axios from "axios";
 import { useState } from "react";
 
+interface productBody {
+  id: number,
+  title: string,
+  description: string
+}
+
 export const useAPI = () => {
+  const [allProducts, setAllProducts] = useState<productBody[]>();
+
   
+  const getAllProducts = async () => {
+    try {
+      const response = await fetch('https://dummyjson.com/products');
+      const data = await response.json();
+      setAllProducts(data.products);
+    }
+    catch (error) {
+      return console.log(error);
+    }
+  }
+
+  return {
+    getAllProducts, 
+    allProducts
+  }
 }
--- src/pages/LolItemListPage/LolItemListPage.tsx
+++ src/pages/LolItemListPage/LolItemListPage.tsx
@@ -1,8 +1,29 @@
+import { useAPI } from "../../API/useAPI";
+import { useEffect } from "react";
+
 const LolItemListPage = () => {
+
+  const { getAllProducts, allProducts } = useAPI(); // wywołanie funkcji useApi ktora zwraca podane rzeczy // 
+
+  useEffect(() => {
+    getAllProducts()
+  }, [])
+
 
   return (
     <div>
       <h1>Lista przedmiotów</h1>
+      <div>
+        {
+          allProducts == undefined ? (
+            <div>allProduct is undefined</div>
+          ) : (
+            allProducts.map((product) => {
+            return <p key={product.id}>{product.title}</p>;
+          } )
+          )
+        }
+      </div>
     </div>
   );
 }
